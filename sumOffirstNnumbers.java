b048f866458bc087ff2d5fa3e69cd71215e0b0c2
Reyansh singh <tribhuvannath4567@gmail.com>
2024-01-18 01:52:25 +0530

    Modified on 1:52

---
+++ sumOffirstNnumbers.java
@@ -0,0 +1,20 @@
+// Java program to find the sum of first N number
+
+import java.util.Scanner;
+
+class sum {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            try {
+                System.out.println("Enter a value :");
+                int n = sc.nextInt();
+                int sum = (n * (n + 1) / 2);
+                System.out.println("The sum of first " + n + " number is " + sum);
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Enter a valid input ");
+            }
+
+        }
+    }
+}
